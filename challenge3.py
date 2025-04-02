281ef9b18c966d23db4b092496185beeca61de24
rfrankeltcsbell <bellmore+rfrankel@thecoderschool.com>
2025-04-02 20:32:50 +0000

    update3.py

---
+++ challenge3.py
@@ -0,0 +1,5 @@
+radius = 1
+pi = 3.14
+circumference = 2* pi * radius
+
+print ("The circumference of the circle is:",circumference)
--- expense.py
+++ expense.py
@@ -47,16 +47,67 @@ def view_expenses():
     print("\nYour expenses:")
     for expense in expenses:
         print(expense["name"]+ "-$"+ str(expense["amount"]) + expense["category"])
+def summarize_expense():
+    expenses= load_expenses()
 
+    if not expenses: 
+        print("No expenses recorded yet.")
+        return 
+    
+    categories= {}
+    for expense in expenses:
+        category= expense ["category"]
+        amount = expense ["amount"]
+        categories [category]= categories.get(category,0 + amount)
+
+    print("\n Expense Summary by Category:")
+    for category, total in categories.items():
+        print(category +":$" + str(round(total,2)))
+
+def delete_expenses():
+    expenses = load_expenses()
+
+    if not expenses: 
+        print("No expenses to delete.")
+        return
+
+    print("\nYour expenses:")
+    for i in range (len(expenses)):
+        e = expenses[i]
+        print(str(i + 1)+ "."+e["name"]+"-$"+ str(e["amount"]))
+
+        user_input= input("Enter the number of expense we want to delete: ")
+    
+        if user_input.isdigit():
+            index = int(user_input) - 1
+
+            if 0 <= index < len(expenses):
+                deleted = expenses.pop(index)
+                save_expenses(expenses)
+                print("Deleted:"+ deleted["name"]+"-$"+ str(deleted["amount"]))
+            else: 
+                print("That number does not exist")
+        else:  
+            print("Please enter a number")
 # MAIN
 while True:
     print ("/nSimple Expense Tracker")
     print("1. Add Expense")
 
 
-    choice = input ("Select an option (1) ").strip()
+    choice = input ("Select an option (1-4) ").strip()
 
     if choice == "1":
         add_expense()
-    if choice == "2":
+ 
+    elif choice == "2":
         view_expenses()
+    elif choice =="3":
+        summarize_expense()
+    
+    elif choice =="4":
+        print("\nGoodbye!")
+        break
+    else:
+        print("Invaild Choice,Try again")
+
